d9660535b658aa6e14efe957d14027f68e7def97
jmoore6364 <jmoore6364@yahoo.com>
2018-03-22 18:37:45 -0700

    Create an initial simple dataset to work with. I will read the csv file in later if I have time.

--- Program.cs
+++ Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace StudentQuestions
 {
+    
     class Program
     {
+        
+
+        static List<dynamic> GetQuestions()
+        {
+            return new List<dynamic>()
+            {
+                new { strand_id = 1,  standard_id = 1, question_id = 1},
+                new { strand_id = 1,  standard_id = 1, question_id = 2},
+                new { strand_id = 1,  standard_id = 2, question_id = 3},
+                new { strand_id = 1,  standard_id = 2, question_id = 4},
+                new { strand_id = 1,  standard_id = 3, question_id = 5},
+                new { strand_id = 2,  standard_id = 4, question_id = 6},
+                new { strand_id = 2,  standard_id = 4, question_id = 7},
+                new { strand_id = 2,  standard_id = 4, question_id = 8},
+                new { strand_id = 2,  standard_id = 5, question_id = 9},
+                new { strand_id = 2,  standard_id = 5, question_id = 10}
+            };
+            
+        }
+
         static void Main(string[] args)
         {
+            var questions = GetQuestions();
+            
+            Console.ReadKey();
         }
     }
 }
